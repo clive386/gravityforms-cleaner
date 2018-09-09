a960a2a268767f507e8c03c8a8ef4f5b8e763f3e
Clive <github@randomgibberish.net>
2018-09-09 10:28:43 +0100

    Initial plugin boilerplate

---
+++ gravityforms-cleaner.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Plugin Name: Gravity Forms Submission Cleaner
+ * Description: Easily clean old or expired submissions from Gravity Forms
+ *
+ * Plugin URI: https://github.com/clive386/gravityforms-cleaner
+ * Version: 0.0.1
+ *
+ * Author: Clive Martin
+ */
+
